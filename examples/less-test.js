980d9c2f49b97a330052294cd59f982cf6c5b974
Michael Schwartz <mykesx@gmail.com>
2014-10-05 07:05:23 -0700

    added example program

---
+++ examples/less-test.js
@@ -0,0 +1,41 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mschwartz
+ * Date: 7/3/13
+ * Time: 9:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var less = require('..').compile;
+
+console.dir(less);
+builtin.process.exit(0);
+
+var source = [
+'@base: #f938ab;',
+'',
+'.box-shadow(@style, @c) when (iscolor(@c)) {',
+'    box-shadow:         @style @c;',
+'    -webkit-box-shadow: @style @c;',
+'    -moz-box-shadow:    @style @c;',
+'}',
+'.box-shadow(@style, @alpha: 50%) when (isnumber(@alpha)) {',
+'.box-shadow(@style, rgba(0, 0, 0, @alpha));',
+'}',
+'.box {',
+'    color: saturate(@base, 5%);',
+'    border-color: lighten(@base, 30%);',
+'    div { .box-shadow(0 0 5px, 30%) }',
+'}'
+].join('\n');
+
+//var parser = new less.Parser();
+//parser.parse(source, function(e, root) {
+//    if (e) {
+//        console.dir(e);
+//    }
+//    else {
+//        console.log(root.toCSS());
+//    }
+//});
+//
